aa8db5b8b76df0261b312869693676fde23d3d5a
Nupur <nupursangwan564@gmail.com>
2026-08-05 21:30:51 +0530

    Enhance day1.py with multiple operator examples
    Added various arithmetic, logical, comparison, identity, and bitwise operations with user input.

--- day1.py
+++ day1.py
@@ -3,15 +3,47 @@
 #python variables
 a=int(input("enter a number"))
 b=int(input("enter a number"))
-a+=2
-print(a)
-b*=1
-print(b)
-c=5
-d=8
-print("a*c=",a*c)
-print("b/c=",b/c)
+print("addition of a and b is : ",a+b)
+print("subtraction of a and b is : ",a-b)
+print("multiplication of a and b is : ",a*b)
+print("division of a and b is : ",a/b)
+print("floor division of a and b is : ",a//b)
 
-#logical operators
-print("a=>b and c>=b",a>=b and c<=b)
+#comparison operator
+c=int(input("enter a number"))
+d=int(input("enter a number"))
+print(" c == d ",c==d)
+print("c<=d : ",c<=d)
+print("c>=d: ",c>=d)
+print("c!=d ",c!=d)
+
+#logical operator
+e=int(input("enter a number"))
+f=int(input("enter a number"))
+g=int(input("enter a number"))
+print("(e>f)and(e<g))",(e>f)and(e<g))
+
+#identity operator
+h=int(input("enter a number"))
+i=int(input("enter a number"))
+j=int(input("enter a number"))
+print(" h is i",h is i)
+print("h is not g",h is not g)
+
+#bitwise operator
+p=int(input("enter a number"))
+q=int(input("enter a number"))
+print("p&q",p&q)
+print("p|q",p|q)
+print("p xor q",p^q)
+print(" not a",~a)
+
+#Assignment 
+m=int(input("enter a number"))
+n=int(input("enter a number"))
+m+=2
+print(m)
+n*=5
+print(n)
+
 
